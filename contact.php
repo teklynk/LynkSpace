bc37c8cdb7fd3f6e17c021271b829a196ac8aa14
Ryan Jones <ryanjones153@gmail.com>
2016-01-28 13:13:57 -0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -54,7 +54,7 @@ include 'includes/header.php';
         <div class="row">
             <div class="col-md-8">
                 <h3>Send us a Message</h3>
-                <form name="sentMessage" id="contactForm" method="post" action="mail/sendmail.asp">
+                <form name="sentMessage" id="contactForm" method="post" action="mail/contact_me.php">
                     <div class="control-group form-group">
                         <div class="controls">
                             <label>Full Name:</label>
