16d427c092cb1e311c329a1a8b95f16a01d7c067
Ryan Jones <ryanjones153@gmail.com>
2016-05-17 13:54:01 -0400

    Update header.php

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -230,7 +230,7 @@ if (isset($_SESSION["loggedIn"])) {
 //Redirect user if session not set
 if (basename($_SERVER['PHP_SELF'])!='index.php') {
     if (basename($_SERVER['PHP_SELF'])!='install.php') {
-        if ($_SESSION['timeout'] + $sessionTimeout * 1 < time()) { //60 minute session timeout
+        if ($_SESSION['timeout'] + $sessionTimeout * 60 < time()) { //60 minute session timeout
 
             if (!$_SESSION["loggedIn"]) {
     	       echo "<script>window.location.href='index.php';</script>"; //this works.
