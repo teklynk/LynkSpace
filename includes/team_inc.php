e34e1ae5a2f06b8ef4858ddd387d0673d48a36c4
Ryan Jones <ryanjones153@gmail.com>
2016-12-02 21:37:38 -0500

    team styling

--- includes/team_inc.php
+++ includes/team_inc.php
@@ -12,13 +12,13 @@ getTeam();
 		
 		if (!empty($teamHeading)) {
 			echo "<div class='col-lg-12'>";
-			echo "<h3 class='page-header team'>".$teamHeading."</h3>";
+			echo "<h1 class='team'>".$teamHeading."</h1>";
 			echo "</div>";
 		}
 
         if (!empty($teamBlurb)) {
             echo "<div class='col-lg-12'>";
-            echo "<p class='text-center'>".$teamBlurb."</p>";
+            echo "<h3>".$teamBlurb."</h3>";
             echo "</div>";
         }
 
