1bbcc234652ea04202070b845fef457687e2638f
teklynk <ryanjones153@gmail.com>
2016-12-06 17:09:42 -0500

    styling changes to search tabs

--- includes/searchpac_inc.php
+++ includes/searchpac_inc.php
@@ -4,8 +4,6 @@ if (!defined('inc_access')) {
    die('Direct access not permitted');
 
     getSetup(); //from functions.php
-
-    $isDefault = '';
 }
 ?>
 <div class="row" id="searchpac">
@@ -45,30 +43,22 @@ if (!defined('inc_access')) {
                         <?php
                         if ($setupLs2pac == 'true') {
                             if ($setupSearchDefault == 1) {
-                                $isDefault = 'active';
+                                $isDefault1 = 'active';
                             } else {
-                                $isDefault = '';
+                                $isDefault1 = '';
                             }
                         ?>
                         <!-- LS2PACSearch Form -->
-                        <div class="tab-pane fade in <?php echo $isDefault; ?>" id="tab1default">
+                        <div class="tab-pane fade in <?php echo $isDefault1; ?>" id="tab1default">
                             <form name="ls2pacForm" method="post" onSubmit="return getSearchString(3, this, TLCDomain, TLCConfig, TLCBranch, 'ls2', false);">
                                 <div id="custom-search-input">
                                     <div class="input-group col-md-12">
-
                                         <input type="text" class="form-control" name="term" placeholder="LS2PAC" />
                                         <span class="input-group-btn">
                                             <button class="btn btn-danger" type="submit">
                                                 <span class="glyphicon glyphicon-search"></span>
                                             </button>
                                         </span>
-
-                                    </div>
-                                    <div class="input-group col-md-12 text-center">
-                                    <?php
-                                    //EXAMPLE: getNav($navSection,$dropdown,$pull)
-                                    getNav('Search','true','left');
-                                    ?>
                                     </div>
                                 </div>
                             </form>
@@ -78,30 +68,22 @@ if (!defined('inc_access')) {
 
                         if ($setupLs2kids == 'true') {
                             if ($setupSearchDefault == 2) {
-                                $isDefault = 'active';
+                                $isDefault2 = 'active';
                             } else {
-                                $isDefault = '';
+                                $isDefault2 = '';
                             }
                         ?>
                         <!-- LS2Kids Search Form -->
-                        <div class="tab-pane fade in <?php echo $isDefault; ?>" id="tab2default">
+                        <div class="tab-pane fade in <?php echo $isDefault2; ?>" id="tab2default">
                             <form name="ls2kidspacForm" method="post" onSubmit="return getSearchString(3, this, TLCDomain, TLCConfig, TLCBranch, 'ls2', false);">
                                 <div id="custom-search-input">
                                     <div class="input-group col-md-12">
-
                                         <input type="text" class="form-control" name="term" placeholder="LS2Kids" />
                                         <span class="input-group-btn">
-                                        <button class="btn btn-danger" type="submit">
-                                            <span class="glyphicon glyphicon-search"></span>
-                                        </button>
-                                    </span>
-
-                                    </div>
-                                    <div class="input-group col-md-12 text-center">
-                                        <?php
-                                        //EXAMPLE: getNav($navSection,$dropdown,$pull)
-                                        getNav('Search','true','left');
-                                        ?>
+                                            <button class="btn btn-danger" type="submit">
+                                                <span class="glyphicon glyphicon-search"></span>
+                                            </button>
+                                        </span>
                                     </div>
                                 </div>
                             </form>
@@ -113,6 +95,12 @@ if (!defined('inc_access')) {
                     </div>
                 </div>
 
+            </div>
+            <div class="input-group col-md-12 text-center">
+                <?php
+                //EXAMPLE: getNav($navSection,$dropdown,$pull)
+                getNav('Search','true','left');
+                ?>
             </div>
         </div>
     </div>
