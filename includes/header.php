6890ab9d86335d26a7b49d3d16bec6ac48a21c45
teklynk <ryanjones153@gmail.com>
2016-12-08 15:50:38 -0500

    fixed loggoff redirect

--- includes/header.php
+++ includes/header.php
@@ -12,7 +12,7 @@
     <html lang="en">
     <head>
     <meta charset="utf-8">
-    <meta http-equiv="refresh" content="3600;URL='index.php?loc_id=<?php echo $_GET['loc_id'];?>'">
+    <meta http-equiv="refresh" content="3600;URL=index.php?loc_id=<?php echo $_GET['loc_id'];?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="<?php echo $setupDescription;?>">
--- includes/searchpac_inc.php
+++ includes/searchpac_inc.php
@@ -95,7 +95,7 @@ if (!defined('inc_access')) {
                 </div>
 
             </div>
-            <div class="input-group col-md-12 text-center">
+            <div class="input-group col-md-12 text-center center-block"">
                 <?php
                 //EXAMPLE: getNav($navSection,$dropdown,$pull)
                 getNav('Search','false','center');
