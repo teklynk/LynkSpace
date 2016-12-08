fffe81f1bf2ce6d465f8e37a439598e8ce77e2f6
teklynk <ryanjones153@gmail.com>
2016-12-08 12:09:54 -0500

    fixed password reset email processor

--- admin/mail/passwordreset.php
+++ admin/mail/passwordreset.php
@@ -35,15 +35,15 @@ if (isset($_POST['user_name'])) {
     } else {
         // Create the email and send the message
         $email_subject = "Website User Account Form:  $user_name";
-        $email_body = "Your password has been reset.\n\n" . "Here are the details:\n\nUser Name: $user_name\n\nEmail: $email_address\n\nTemp Password: $temp_password\n\n\"";
-        $headers = "From: $email_address\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-        $headers .= "Reply-To: $email_address";
+        $email_body = "Your password has been reset.\n\n" . "Here are the details:\n\nUser Name: $user_name\n\nEmail: $email_address\n\nTemp Password: $temp_password\n\n";
+        $headers = "From: noreply@dev-vm.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+        $headers .= "Reply-To: noreply@dev-vm.com";
 
         //Update user password with temp_password where email and username match
         $contactUpdate = "UPDATE users SET username='".$user_name."', password=password('".$temp_password."') WHERE email='".$email_address."' AND username='".$user_name."' ";
         mysqli_query($db_conn, $contactUpdate);
 
-        mail($email_subject, $email_body, $headers);
+        mail($email_address, $email_subject, $email_body, $headers);
 
         echo "<script>window.location.href='$redirectPage';</script>";
         return true;
