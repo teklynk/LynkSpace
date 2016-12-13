b24ee8d3182d78602c9580a32414f1da7e05ccc4
teklynk <ryanjones153@gmail.com>
2016-12-13 17:47:46 -0500

    code cleanup

--- js/functions.js
+++ js/functions.js
@@ -1,14 +1,13 @@
 $(document).ready(function() {
-
-    //Page Load/Performance Checker
-    window.onload = function() {
-        //var loadTime = ((window.performance.timing.domComplete- window.performance.timing.navigationStart)/1000)+" sec.";
-        var loadTime = window.performance.timing.domContentLoadedEventEnd-window.performance.timing.navigationStart;
-        console.log('Page load time is '+ loadTime);
-        console.log(window.performance);
-    };
-
-    //Responsive classes
-    $('.content img').addClass('img-responsive');
+    //Add responsive classes to wysiwyg elements
+    $('.content img').addClass('img-responsive hidden-xs');
     $('.content iframe').addClass('embed-responsive-item iframe');
 });
+
+//Page Load/Performance Checker
+window.onload = function() {
+    //var loadTime = ((window.performance.timing.domComplete- window.performance.timing.navigationStart)/1000)+" sec.";
+    var loadTime = window.performance.timing.domContentLoadedEventEnd-window.performance.timing.navigationStart;
+    console.log('Page load time is '+ loadTime);
+    console.log(window.performance);
+};
