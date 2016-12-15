670eda62b016f4eabfb768b5bbcc1d15c11a267d
teklynk <ryanjones153@gmail.com>
2016-12-15 09:21:49 -0500

    sticky footer onload event

--- js/functions.js
+++ js/functions.js
@@ -5,15 +5,17 @@ $(document).ready(function() {
     $('.content iframe').addClass('embed-responsive-item iframe');
 
     // Sticky Footer initial page load
-    var bodyHeight = $('body').height();
-    var navbarHeight = $('.navbar-static-top').height();
-    var bannerHeight = $('.bannerwrapper').height();
-    var footerHeight = $('.footer').height();
-    var calcContentHeight = bodyHeight - navbarHeight - bannerHeight - footerHeight - 131; //change last value to compensate for padding.
+    $(window).load(function() {
+        var bodyHeight = $('body').height();
+        var navbarHeight = $('.navbar-static-top').height();
+        var bannerHeight = $('.bannerwrapper').height();
+        var footerHeight = $('.footer').height();
+        var calcContentHeight = bodyHeight - navbarHeight - bannerHeight - footerHeight - 131; //change last value to compensate for padding.
 
-    if (calcContentHeight > 0) {
-        $('.content').css({'min-height': calcContentHeight});
-    }
+        if (calcContentHeight > 0) {
+            $('.content').css({'min-height': calcContentHeight});
+        }
+    });
 
     // Sticky Footer on browser resize
     $(window).resize(function(){
@@ -26,7 +28,6 @@ $(document).ready(function() {
         if (calcContentHeight > 0) {
             $('.content').css({'min-height': calcContentHeight});
         }
-
     });
 
 });
