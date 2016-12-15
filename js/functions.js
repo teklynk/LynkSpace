1844ba4e9f125104028286f72846a8da523dd945
teklynk <ryanjones153@gmail.com>
2016-12-15 09:57:14 -0500

    refactored sticky footer js

--- js/functions.js
+++ js/functions.js
@@ -4,21 +4,8 @@ $(document).ready(function() {
     $('.content img').addClass('img-responsive');
     $('.content iframe').addClass('embed-responsive-item iframe');
 
-    // Sticky Footer initial page load
-    $(window).load(function() {
-        var bodyHeight = $('body').height();
-        var navbarHeight = $('.navbar-static-top').height();
-        var bannerHeight = $('.bannerwrapper').height();
-        var footerHeight = $('.footer').height();
-        var calcContentHeight = bodyHeight - navbarHeight - bannerHeight - footerHeight - 131; //change last value to compensate for padding.
-
-        if (calcContentHeight > 0) {
-            $('.content').css({'min-height': calcContentHeight});
-        }
-    });
-
-    // Sticky Footer on browser resize
-    $(window).resize(function(){
+    // Sticky Footer initial page load and resize
+    $(window).on('load resize', function() {
         var bodyHeight = $('body').height();
         var navbarHeight = $('.navbar-static-top').height();
         var bannerHeight = $('.bannerwrapper').height();
