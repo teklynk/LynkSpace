84e484ceb6664993bea9bd62462f65eab48b395c
teklynk <ryanjones153@gmail.com>
2017-01-25 11:19:05 -0500

    changed password hash to SHA

--- admin/mail/passwordreset.php
+++ admin/mail/passwordreset.php
@@ -13,8 +13,6 @@ if ($_POST['user_name'] && $_POST['user_email'] && $_POST['not_robot'] == 'e6a52
     //if an error occurs
     $errorPage = "../index.php?forgotpassword=true&msgsent=error";
 
-    //*7561F5295A1A35CB8E0A7C46921994D383947FA5 = r00t
-
     $user_name = trim($_POST['user_name']);
     $email_address = trim($_POST['user_email']);
     $temp_password = $_SESSION['temp_password'];
@@ -41,7 +39,7 @@ if ($_POST['user_name'] && $_POST['user_email'] && $_POST['not_robot'] == 'e6a52
         $headers .= "Reply-To: noreply@dev-vm.com";
 
         //Update user password with temp_password where email and username match
-        $contactUpdate = "UPDATE users SET username='" . $user_name . "', password=password('" . $temp_password . "') WHERE email='" . filter_var($email_address, FILTER_VALIDATE_EMAIL) . "' AND username='" . $user_name . "' ";
+        $contactUpdate = "UPDATE users SET username='" . $user_name . "', password=SHA1('" . $temp_password . "') WHERE email='" . filter_var($email_address, FILTER_VALIDATE_EMAIL) . "' AND username='" . $user_name . "' ";
         mysqli_query($db_conn, $contactUpdate);
 
         mail($email_address, $email_subject, $email_body, $headers);
