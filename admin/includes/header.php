2f743aab16f53deda146c27d0c626351399592fa
teklynk <ryanjones153@gmail.com>
2017-01-26 17:07:30 -0500

    code cleanup

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -280,11 +280,9 @@ session_start();
 
 if (!$_SESSION['loggedIn']) {
     if (basename($_SERVER['PHP_SELF']) != 'index.php') {
-        //if (basename($_SERVER['PHP_SELF']) != 'install.php') {
-            echo "Not signed in!";
-            header("Location: index.php?logout=true");
-            echo "<script>window.location.href='index.php?logout=true';</script>";
-        //}
+        echo "Not signed in!";
+        header("Location: index.php?logout=true");
+        echo "<script>window.location.href='index.php?logout=true';</script>";
     }
 }
 
