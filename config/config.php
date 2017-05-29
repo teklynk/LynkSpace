72cb5738e7bfd92f7f062ee0c334f08f9b6d059c
Ryan Jones <ryanjones153@gmail.com>
2017-05-29 02:11:05 -0400

    changed subDirectory path

--- config/config.php
+++ config/config.php
@@ -27,16 +27,15 @@ if ($_SERVER['HTTPS'] == true || $_SERVER['HTTPS'] == 'on') {
 }
 
 //Get Sub-folder name
-$subDirs = $_SERVER['REQUEST_URI'];
-$subPath = parse_url($subDirs, PHP_URL_PATH);
-$subDirectory = explode('/', $subPath)[1];
-echo $subDirectory;
-echo count($subDirectory);
+$subURL = $_SERVER['REQUEST_URI'];
+$subPath = parse_url($subURL, PHP_URL_PATH);
+$subDir = explode('/', $subPath)[1];
+$subDir = trim($subDir);
 
-if (strpos($subDirectory, 'php') || strpos($subDirectory, 'html')){
+if (strpos($subDir, 'admin') !== false || strpos($subDir, 'php') !== false) {
     $subDirectory = '';
 } else {
-    $subDirectory = '/'.$subDirectory;
+    $subDirectory = '/'.$subDir;
 }
 
 //Theme value
