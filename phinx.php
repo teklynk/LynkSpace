da17abc59b7e9e59e63c12d2fc5192adc187d70d
teklynk <ryanjones153@gmail.com>
2017-06-19 13:07:25 -0400

    added Phinx migration

---
+++ phinx.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once __DIR__.'/config/dbconn.php';
+
+return
+    [
+        'paths' => [
+            'migrations' => '%%PHINX_CONFIG_DIR%%/migration',
+        ],
+        'environments' =>
+            [
+                'default_database' => '*** CHOOSE AN ENVIRONMENT ***',
+                'default_migration_table' => 'phinxlog',
+                'development' =>
+                    [
+                        'adapter' => 'mysql',
+                        'host' => db_servername,
+                        'name' => db_name,
+                        'user' => db_username,
+                        'pass' => db_password,
+                        'port' => 3306,
+                        'charset' => 'utf8',
+                        'collation' => 'utf8_unicode_ci',
+                    ],
+                'staging' =>
+                    [
+                        'adapter' => 'mysql',
+                        'host' => db_servername,
+                        'name' => db_name,
+                        'user' => db_username,
+                        'pass' => db_password,
+                        'port' => 3306,
+                        'charset' => 'utf8',
+                        'collation' => 'utf8_unicode_ci',
+                    ],
+                'production' =>
+                    [
+                        'adapter' => 'mysql',
+                        'host' => db_servername,
+                        'name' => db_name,
+                        'user' => db_username,
+                        'pass' => db_password,
+                        'port' => 3306,
+                        'charset' => 'utf8',
+                        'collation' => 'utf8_unicode_ci',
+                    ],
+            ],
+    ];
