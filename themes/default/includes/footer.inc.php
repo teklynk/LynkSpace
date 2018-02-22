c9c57d9ef9e9a5df8e01b9042af61a5a7c9da332
teklynk <ryanjones153@gmail.com>
2018-02-22 08:52:22 -0500

    removed commented code

--- themes/default/includes/footer.inc.php
+++ themes/default/includes/footer.inc.php
@@ -43,7 +43,6 @@ if (!defined('inc_access')) {
 <?php if (basename($_SERVER['PHP_SELF']) == 'index.php') { ?>
     <!-- Script to Activate the Carousel -->
     <script type="text/javascript">
-        //toggleSrc(url, loc_id, index)
         toggleSrc('<?php echo $hottitlesLoadFirstUrl; ?>', <?php echo $hottitlesLocID; ?>, 1);
     </script>
 <?php } ?>
