45d80ffa85334caf75e95cd3c9ed1294de512c5c
teklynk <ryanjones153@gmail.com>
2018-09-30 12:49:46 -0400

    csrf function catches post actions

--- admin/mail/passwordreset.php
+++ admin/mail/passwordreset.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-if ($_POST['user_name'] && $_POST['user_email'] && $_SESSION['file_referrer'] == 'index.php' && $_POST['csrf'] == $_SESSION['unique_referrer']) {
+if ($_POST['user_name'] && $_POST['user_email'] && $_SESSION['file_referrer'] == 'index.php') {
 
     require_once(__DIR__ . '/../../config/config.php');
     require_once(__DIR__ . '/../core/functions.php');
