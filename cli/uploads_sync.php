50f54003bbc9f78bc0b318abe773249d1672d43d
teklynk <ryanjones153@gmail.com>
2019-03-31 22:42:54 -0400

    WIP cli uploads_sync

--- cli/uploads_sync.php
+++ cli/uploads_sync.php
@@ -1,25 +1,37 @@
 #!/usr/bin/php
 <?php
-function dirToArray($dir, $fileIgnoreArray = array()) {
+define( 'ALLOW_INC', true );
 
-    $result = array();
+require_once (__DIR__ . '/../htdocs/config/config.php');
+require_once (__DIR__ . '/../htdocs/admin/core/functions.php');
 
-    $cdir = scandir($dir);
-    foreach ($cdir as $key => $value) {
-        if (!in_array($value,array(".",".."))) {
-            if ( in_array( $value, $fileIgnoreArray ) ) {
-                continue;
-            }
-            if (is_dir($dir . DIRECTORY_SEPARATOR . $value)) {
-                $result[$value] = dirToArray($dir . DIRECTORY_SEPARATOR . $value);
-            } else {
-                $result[] = $value;
-            }
-        }
+$uploadsPath = __DIR__ . '/../htdocs/uploads';
+
+$uploadsArray = dirToArray($uploadsPath, array('.gitkeep'));
+
+$sqlUploads = mysqli_query( $db_conn, "SELECT * FROM uploads;" );
+$rowUploads = mysqli_fetch_array( $sqlUploads, MYSQLI_ASSOC );
+
+//var_dump($rowUploads['orig_file_name']);
+//die();
+
+
+//TODO: scan sub directories. get folder name/number and use that as a variable for loc_id. foreach sub directory...
+
+
+foreach ($uploadsArray as $upload) {
+    if (!in_array($upload, (array)$rowUploads['orig_file_name'])) {
+
+        //Get file info
+        $fileExt       = strtolower( pathinfo( $uploadsPath . '/' . $upload, PATHINFO_EXTENSION ) ) ?: null;
+        $fileName      = strtolower( pathinfo( $uploadsPath . '/' . $upload, PATHINFO_BASENAME ) ) ?: null;
+        $fileMime      = strtolower( mime_content_type( $uploadsPath . '/' . $upload ) ) ?: null;
+        $fileSize      = filesize( $uploadsPath . '/' . $upload) ?: null;
+
+        $sqlInsertUploads = "INSERT INTO uploads (type, type_id, file_name, orig_file_name, file_data, file_ext, file_mime, file_size, author_name, guid, datetime, loc_id) VALUES ('" . $type . "', " . $type_id . ", '" . $fileName . "', '" . $upload . "', '" . $fileData . "', '" . $fileExt . "', '" . $fileMime . "', " . $fileSize . ", '" . $user . "', '" . getGuid() . "', '" . date( "Y-m-d H:i:s" ) . "', " . loc_id . ");";
+        //mysqli_query( $db_conn, $sqlInsertUploads );
+
+        echo $sqlInsertUploads;
     }
-
-    return $result;
 }
-
-print_r (dirToArray(__DIR__ . '/../htdocs/uploads', array('.gitkeep')));
 ?>
