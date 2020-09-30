4ce73eb507c41cb73eb525784e9c2ed48d25d8a1
teklynk <ryanjones153@gmail.com>
2020-09-29 21:28:15 -0400

    added page contents to page includes

--- htdocs/themes/theme3/includes/pages.inc.php
+++ htdocs/themes/theme3/includes/pages.inc.php
@@ -17,11 +17,9 @@ if ( count($pageArray) > 0 ) {
 
     echo "<div class='row' id='pages'>";
 
-
-
     foreach($pageArray as $pageData) {
 
-        echo "<div class='col-sm-6 col-md-3 col-lg-3 page-item'>";
+        echo "<div class='col-sm-6 col-md-6 col-lg-6 page-item'>";
         echo "<div class='panel panel-default text-center'>";
 
         echo "<div class='panel-body'>";
@@ -35,7 +33,7 @@ if ( count($pageArray) > 0 ) {
         }
 
         if ( ! empty( $pageData['id'] ) ) {
-            echo "<a href='" . $pageData['id'] . "' class='btn btn-primary'>Learn More</a>";
+            echo "<a href='page.php?page_id=" . $pageData['id'] . "&loc_id=" . $pageData['loc_id'] . "' class='btn btn-primary'>Learn More</a>";
         }
 
         echo "</div>";
