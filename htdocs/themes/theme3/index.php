209c2e314dccc588f0aafb61355b68d980ff9486
teklynk <ryanjones153@gmail.com>
2020-09-30 23:01:59 -0400

    removed banner wrapper

--- htdocs/themes/theme3/index.php
+++ htdocs/themes/theme3/index.php
@@ -9,11 +9,11 @@ require_once( __DIR__ . '/includes/slider.inc.php' );
 
 require_once( __DIR__ . '/includes/featured.inc.php' );
 
-echo "<div class='grad-blue themebase-bgcolor container-fluid search'>";
+/*echo "<div class='grad-blue themebase-bgcolor container-fluid search'>";
 echo "<div class='container bannerwrapper'>";
 
 echo "</div>";
-echo "</div>";
+echo "</div>";*/
 
 require_once( __DIR__ . '/includes/pages.inc.php' );
 
