992e3a6b4207a91208b53a3ed6d6190ec44bbe70
teklynk <ryanjones153@gmail.com>
2021-01-14 19:17:30 -0500

    updated static_site generator

--- cli/static_site.sh
+++ cli/static_site.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
 
+# Requires HTTrack to scrape the site and generate html files and assets.
+# sudo apt install httrack
+
+# Run ./static_site.sh from the command line.
+
+# Define source and destination paths and domain name
+httrackpath=/usr/bin/httrack
 domain=lynkspace.local
 temppath=/var/www/html/LynkSpace/cli/tmp
 staticsitepath=/var/www/html/LynkSpace/htdocs/static
 
-/usr/bin/httrack http://$domain -O $temppath -v -a -w --footer ""
+# Purge the temp/source directory
+#rm -r $temppath/*
 
+#sleep 3
+
+# Run HTTrack
+$httrackpath http://$domain -O $temppath -v -a -w --footer ""
+
+# Find and remove HTTrack comment lines
 find $temppath -type f -name '*.html' -exec sed -i 's#<!-- Added by HTTrack --><meta http-equiv=\"content-type\" content=\"text\/html;charset=UTF-8\" \/><!-- \/Added by HTTrack -->#''#g' {} +
 
+# Purge the destination/static site directory
+#rm -r $staticsitepath/*
+
+#sleep 3
+
+# Copy source/temp path to destination/static path
 cp -r $temppath/$domain/* $staticsitepath/
+
+# Simply ftp/copy the static files to your hosted server.
+# This script can be useful for staging to production deployments.
+# Staging environment could have DocumentRoot set to htdocs/ (with access to the admin panel for editing), while the Production environment only uses htdocs/static.
+# This could also be used for production environments that do not have/allow PHP and MySQL, possibly for security concerns or cost.
